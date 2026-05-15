a17b22463e9617a189e87de8cdf569264cc19a45
maxxxone11-s <maximzadko187@gmail.com>
2026-05-16 01:55:21 +0300

    1.5  test

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import IntegrityError
 from app.schemas.user_schema import UserCreate, UserResponse
 from app.api.deps import get_db
 from app.models.user_model import User
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -30,3 +30,32 @@ async def register_user(
         raise HTTPException(status_code=400, detail="Пользователь с таким email уже существует.")
 
     return user
+
+@router.post("/login")
+async def login_user(
+    email: str,
+    password: str,
+    db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(
+        select(User)
+        .where(email == User.email)
+    )
+
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=400, detail="User not found")
+    
+    verify_password = verify_password(password, user.hashed_password)
+
+    if verify_password:
+        return create_access_token()
+
+    raise HTTPException(status_code=404, detail="Password don't correct")
+    
+def create_access_token():
+    return {"Login": "successful"}
+
+def decode_token():
+    pass
