86404e0494f1199e00398b5da3c8641ca2cf53cd
maxxxone11-s <maximzadko187@gmail.com>
2026-05-16 17:51:55 +0300

    1.9.2 fix

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
-from app.api.routes import health, auth
+from app.api.routes import health, auth, admin, shifts
 
 app = FastAPI()
 
 app.include_router(health.router)
 app.include_router(auth.router)
+app.include_router(admin.router)
+app.include_router(shifts.router)
