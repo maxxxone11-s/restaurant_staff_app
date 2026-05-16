4e8b4dabdf0441698d1d23c9aa92c898b2cbb7f2
maxxxone11-s <maximzadko187@gmail.com>
2026-05-16 16:11:52 +0300

    1.9 Relationship

---
+++ app/models/shift.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey, DateTime, func
+
+from app.core.base import Base
+
+class Shift(Base):
+    __tablename__ = "shifts"
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey=("users.id"),
+        nullable=False
+    )
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        index=True
+    )
+
+    open_shift: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=True
+    )
+
+    closed_shift: Mapped[datetime| None] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True
+    )
+
+    revenue: Mapped[int] = mapped_column(
+        nullable=True
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    user = relationship("User", back_populates="shifts")
