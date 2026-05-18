8086e924a1110874d088cf8ec9bbf9444b012c52
maxxxone11-s <maximzadko187@gmail.com>
2026-05-18 17:24:51 +0300

    1.9.3 fix

--- app/api/routes/shifts.py
+++ app/api/routes/shifts.py
@@ -81,17 +81,18 @@ async def my_shifts(
 
     for shift in shifts_user:
         hours_worked = None
-        if Shift.closed_shift.is_not(None):
+        
+        if shift.closed_shift is not(None):
             delta = shift.closed_shift - shift.open_shift
             hours_worked = delta.total_seconds() / 3600
 
-            response.append({
-                "id": shift.id,
-                "user_id": shift.user_id,
-                "open_shift": shift.open_shift,
-                "closed_shift": shift.closed_shift,
-                "revenue": shift.revenue,
-                "hours_worked": hours_worked
-            })
+        response.append({
+            "id": shift.id,
+            "user_id": shift.user_id,
+            "open_shift": shift.open_shift,
+            "closed_shift": shift.closed_shift,
+            "revenue": shift.revenue,
+            "hours_worked": hours_worked
+        })
 
     return response
